916ff992980d4279d2546d2dda74961512a26534
Mckay <mckaywrigley@gmail.com>
2019-04-30 09:45:00 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
 
 const server = express();
 
@@ -7,6 +7,7 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.post("/api/zoos", (req, res) => {});
 
 const port = 3300;
 server.listen(port, function() {
