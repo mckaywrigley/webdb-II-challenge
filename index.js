6434efba05d2c7d42cf4cbcb942a48e7d95ba803
Mckay <mckaywrigley@gmail.com>
2019-04-30 10:08:01 -0700

    GET zoos

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const helmet = require("helmet");
 const knex = require("knex");
 const sqlite3 = require("sqlite3");
 
+const knexConfig = {
+  client: "sqlite3",
+  connection: {
+    filename: "./data/lambda.sqlite3"
+  },
+  useNullAsDefault: true
+};
+const db = knex(knexConfig);
+
 const server = express();
 
 server.use(express.json());
@@ -11,7 +20,15 @@ server.use(helmet());
 // endpoints here
 server.post("/api/zoos", (req, res) => {});
 
-server.get("/api/zoos", (req, res) => {});
+server.get("/api/zoos", (req, res) => {
+  db("zoos")
+    .then(zoos => {
+      res.status(200).json(zoos);
+    })
+    .catch(err => {
+      res.status(400).json({ error: "Could not find zoos." });
+    });
+});
 
 server.get("/api/zoos/:id", (req, res) => {});
 
